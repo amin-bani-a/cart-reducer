8f7eb4ba9b05be1045fb54450cd24bea84f8f7ec
amin-bani-a <amin.baniamerian67@gmail.com>
2023-08-07 18:03:07 +0400

    add clear cart action

--- src/reducer.js
+++ src/reducer.js
@@ -9,7 +9,13 @@ import {
 } from "./actions";
 
 const reducer = (state, action) => {
-  return state;
+  if (action.type === CLEAR_CART) {
+    return {
+      ...state,
+      cart: new Map(),
+    };
+  }
+  throw new Error(`no matching action type: ${action.type}`);
 };
 
 export default reducer;
