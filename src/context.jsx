bf98e4e1d6cf14e1b29303972b5c9abdc783a6da
amin-bani-a <amin.baniamerian67@gmail.com>
2023-08-07 17:51:49 +0400

    add Map() to show the data

---
+++ src/context.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useReducer } from "react";
+import reducer from "./reducer";
+import cartItems from "./data";
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  DISPLAY_ITEMS,
+  LOADING,
+} from "./actions";
+
+const AppContext = createContext();
+
+const initialState = {
+  loading: false,
+  cart: new Map(cartItems.map((item) => [item.id, item])),
+};
+
+export const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
---
+++ src/reducer.js
@@ -0,0 +1,15 @@
+import { useReducer } from "react";
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  DISPLAY_ITEMS,
+  LOADING,
+} from "./actions";
+
+const reducer = (state, action) => {
+  return state;
+};
+
+export default reducer;
